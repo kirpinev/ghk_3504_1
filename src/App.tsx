7c838f0c9068cca40afe153cb2d30a3e5b9d7310
Igor Kirpinev <kirpinevigor@yandex.ru>
2024-11-03 12:52:37 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,91 @@
+import { ButtonMobile } from "@alfalab/core-components/button/mobile";
+
+import { Typography } from "@alfalab/core-components/typography";
+
+import card from "./assets/card.png";
+import { LS, LSKeys } from "./ls";
+import { appSt } from "./style.css";
+import { ThxLayout } from "./thx/ThxLayout";
+import { Gap } from "@alfalab/core-components/gap";
+
+export const App = () => {
+  const submit = () => {
+    LS.setItem(LSKeys.ShowThx, true);
+  };
+
+  if (LS.getItem(LSKeys.ShowThx, false)) {
+    return <ThxLayout />;
+  }
+
+  return (
+    <>
+      <div className={appSt.container}>
+        <div className={appSt.box}>
+          <Typography.Text tag="p" view="primary-medium" weight="bold">
+            Карта для ребёнка
+          </Typography.Text>
+          <img src={card} alt="Карта для ребенка" style={{ width: "100%" }} />
+        </div>
+
+        <Gap size={32} />
+
+        <Typography.TitleResponsive
+          font="system"
+          tag="h3"
+          view="medium"
+          weight="regular"
+          className={appSt.productsTitle}
+        >
+          Детская карта
+        </Typography.TitleResponsive>
+        <Typography.Text tag="p" view="primary-medium" color="secondary">
+          Бесплатная карта с кэшбэком
+        </Typography.Text>
+
+        <Gap size={12} />
+
+        <div className={appSt.products}>
+          <div className={appSt.bonus}>
+            <Typography.Text tag="p" view="primary-large" weight="bold">
+              5%
+            </Typography.Text>
+            <Typography.Text tag="p" view="primary-small" color="secondary">
+              В трёх категориях на выбор и 1% на все — всегда
+            </Typography.Text>
+          </div>
+          <div className={appSt.bonus}>
+            <Typography.Text tag="p" view="primary-large" weight="bold">
+              0 ₽
+            </Typography.Text>
+            <Typography.Text tag="p" view="primary-small" color="secondary">
+              Выпуск и обслуживание
+            </Typography.Text>
+          </div>
+        </div>
+
+        <Gap size={20} />
+
+        <Typography.TitleResponsive
+          font="system"
+          tag="h3"
+          view="small"
+          weight="regular"
+          className={appSt.productsTitle}
+        >
+          Карта в приложении у вас и ребёнка
+        </Typography.TitleResponsive>
+        <Typography.Text tag="p" view="primary-medium" color="secondary">
+          Ребёнок видит историю своих покупок и учится управлять тратами
+        </Typography.Text>
+      </div>
+
+      <Gap size={96} />
+
+      <div className={appSt.bottomBtn}>
+        <ButtonMobile block view="primary" href="" onClick={submit}>
+          Выбрать
+        </ButtonMobile>
+      </div>
+    </>
+  );
+};
---
+++ src/style.css.ts
@@ -0,0 +1,58 @@
+import { style } from "@vanilla-extract/css";
+
+const container = style({
+  display: "flex",
+  padding: "1rem",
+  flexDirection: "column",
+  // gap: "1rem",
+});
+
+const box = style({
+  textAlign: "center",
+});
+
+const bottomBtn = style({
+  position: "fixed",
+  zIndex: 2,
+  width: "100%",
+  padding: "12px",
+  bottom: 0,
+});
+
+const productsTitle = style({
+  marginBottom: "0.5rem",
+});
+
+const products = style({
+  display: "flex",
+});
+
+const product = style({
+  backgroundColor: "#F2F3F5",
+  borderRadius: "1rem",
+  padding: "1rem",
+  display: "flex",
+  justifyContent: "space-between",
+  gap: "0.2rem",
+});
+
+const productTitle = style({
+  lineHeight: "24px",
+  fontSize: "17px",
+  marginBottom: "0.3rem",
+});
+
+const bonus = style({
+  flex: 1,
+});
+
+export const appSt = {
+  bottomBtn,
+  container,
+  box,
+  productsTitle,
+  products,
+  productTitle,
+  product,
+  bonus,
+};
